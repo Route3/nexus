ee4be3d6fd7023a8870b5c887d5a2c015747d7b2
Lazar Travica <lazar.travica@route3.co>
2025-05-05 20:30:55 +0200

    Removed obsolete recipt RLP encoding test.

--- types/rlp_encoding_test.go
+++ types/rlp_encoding_test.go
@@ -1,94 +1,10 @@
 package types
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-type codec interface {
-	RLPMarshaler
-	RLPUnmarshaler
-}
-
-func TestRLPEncoding(t *testing.T) {
-	cases := []codec{
-		&Header{},
-		&Receipt{},
-	}
-	for _, c := range cases {
-		buf := c.MarshalRLPTo(nil)
-
-		res, ok := reflect.New(reflect.TypeOf(c).Elem()).Interface().(codec)
-		if !ok {
-			t.Fatalf("Unable to assert type")
-		}
-
-		if err := res.UnmarshalRLP(buf); err != nil {
-			t.Fatal(err)
-		}
-
-		buf2 := c.MarshalRLPTo(nil)
-		if !reflect.DeepEqual(buf, buf2) {
-			t.Fatal("[ERROR] Buffers not equal")
-		}
-	}
-}
-
-func TestRLPStorage_Marshall_And_Unmarshall_Receipt(t *testing.T) {
-	addr := StringToAddress("11")
-	hash := StringToHash("10")
-
-	testTable := []struct {
-		name      string
-		receipt   *Receipt
-		setStatus bool
-	}{
-		{
-			"Marshal receipt with status",
-			&Receipt{
-				CumulativeGasUsed: 10,
-				GasUsed:           100,
-				ContractAddress:   &addr,
-				TxHash:            hash,
-			},
-			true,
-		},
-		{
-			"Marshal receipt without status",
-			&Receipt{
-				Root:              hash,
-				CumulativeGasUsed: 10,
-				GasUsed:           100,
-				ContractAddress:   &addr,
-				TxHash:            hash,
-			},
-			false,
-		},
-	}
-
-	for _, testCase := range testTable {
-		t.Run(testCase.name, func(t *testing.T) {
-			receipt := testCase.receipt
-
-			if testCase.setStatus {
-				receipt.SetStatus(ReceiptSuccess)
-			}
-
-			unmarshalledReceipt := new(Receipt)
-			marshaledRlp := receipt.MarshalStoreRLPTo(nil)
-
-			if err := unmarshalledReceipt.UnmarshalStoreRLP(marshaledRlp); err != nil {
-				t.Fatal(err)
-			}
-
-			if !assert.Exactly(t, receipt, unmarshalledReceipt) {
-				t.Fatal("[ERROR] Unmarshalled receipt not equal to base receipt")
-			}
-		})
-	}
-}
 
 func TestRLPUnmarshal_Header_ComputeHash(t *testing.T) {
 	// header computes hash after unmarshalling
